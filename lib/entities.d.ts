b1409de9e93ccdf1c4555ac92f801182ab1c47f7
Daniele Plantera <tango1266@arcor.de>
2021-10-16 13:41:53 +0200

    Test delete type - should be committed from pipeline

--- lib/entities.d.ts
+++
@@ -1,8 +0,0 @@
-export declare abstract class Entity {
-    id: string | number | undefined;
-}
-export declare const EntityObject: {
-    new (): {
-        id: string | number | undefined;
-    };
-};
